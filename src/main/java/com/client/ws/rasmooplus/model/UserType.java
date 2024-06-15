bdccf6df176d6f3e594b76dbbaf7079729f023cb
ANTONIOALGMAR <antonioalgmar@gmail.com>
2024-06-15 01:41:34 -0300

    criando entidades parte 1

---
+++ src/main/java/com/client/ws/rasmooplus/model/UserType.java
@@ -0,0 +1,23 @@
+package com.client.ws.rasmooplus.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name= "user_type")
+public class UserType implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="user_type_id")
+    private long id;
+
+    private String name;
+
+    private String description;
+
+    private String email;
+
+
+}
