031493d3c2f39ed4d3b24b61c7409c666f018a3f
ANTONIOALGMAR <antonioalgmar@gmail.com>
2024-06-17 17:24:07 -0300

    Melhorando nossas handles com o ErrorResponeDto

--- src/main/java/com/client/ws/rasmooplus/exception/handler/rosourceHandler.java
+++ src/main/java/com/client/ws/rasmooplus/exception/handler/rosourceHandler.java
@@ -1,5 +1,6 @@
 package com.client.ws.rasmooplus.exception.handler;
 
+import com.client.ws.rasmooplus.dto.error.ErrorResponseDto;
 import com.client.ws.rasmooplus.exception.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,16 +10,24 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class rosourceHandler {
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<String> notFoundException(NotFoundException n){
+    public ResponseEntity<ErrorResponseDto> notFoundException(NotFoundException n){
         String errorMessage = n.getMessage();
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMessage);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorResponseDto.builder()
+                        .message(n.getMessage())
+                        .httpStatus(HttpStatus.NOT_FOUND)
+                        .statusCode(HttpStatus.NOT_FOUND.value())
+                .build());
 
     }
 
     @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<String> badRequestException(BadRequestException b){
+    public ResponseEntity<ErrorResponseDto> badRequestException(BadRequestException b){
         String errorMessage = b.getMessage();
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponseDto.builder()
+                .message(b.getMessage())
+                .httpStatus(HttpStatus.BAD_REQUEST)
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .build());
 
     }
 }
